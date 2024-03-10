d9aea11e4292fe60119bcbd97a1a47c2f304da35
Jefferson Nunes <jeffersonvnunes@gmail.com>
2024-03-09 21:13:58 -0300

    Teste

--- srv/novoPagamento/index.mjs
+++ srv/novoPagamento/index.mjs
@@ -1,23 +1,4 @@
-import { SQSEvent, Context, SQSHandler, SQSRecord } from "aws-lambda";
-
-export const functionHandler: SQSHandler = async (
-    event: SQSEvent,
-    context: Context
-): Promise<void> => {
-    for (const message of event.Records) {
-        await processMessageAsync(message);
-    }
-    console.info("done");
+export const handler = async (event, context) => {
+    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+    return context.logStreamName;
 };
-
-async function processMessageAsync(message: SQSRecord): Promise<any> {
-    try {
-        console.log(`Processed message ${message.body}`);
-        // TODO: Do interesting work based on the new message
-        await Promise.resolve(1); //Placeholder for actual async work
-    } catch (err) {
-        console.error("An error occurred");
-        throw err;
-    }
-}
-
