4e4c3e2b7b8e6bcdceed7439eefdb604ff76b72f
Jefferson Nunes <jeffersonvnunes@gmail.com>
2024-03-09 21:35:27 -0300

    teste

--- srv/novoPagamento/index.mjs
+++ srv/novoPagamento/index.mjs
@@ -1,10 +1,21 @@
 export const handler = async (event, context) => {
     console.log("EVENT: \n" + JSON.stringify(event, null, 2));
     for (const message of event.Records) {
-        console.log("Message: \n" + JSON.stringify(message.body, null, 2));
+        await processMessageAsync(message);
     }
     console.info("done");
     return context.logStreamName;
 };
+
+async function processMessageAsync(message): Promise<any> {
+    try {
+        console.log("Message: \n" + JSON.stringify(message.body, null, 2));
+        // TODO: Do interesting work based on the new message
+        await Promise.resolve(1); //Placeholder for actual async work
+    } catch (err) {
+        console.error("An error occurred");
+        throw err;
+    }
+}
 
 
