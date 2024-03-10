04b21b51a26653af6d58ca425bf65103152c5063
Jefferson Nunes <jeffersonvnunes@gmail.com>
2024-03-09 21:40:17 -0300

    teste

--- srv/novoPagamento/index.mjs
+++ srv/novoPagamento/index.mjs
@@ -8,14 +8,7 @@ export const handler = async (event, context) => {
 };
 
 async function processMessageAsync(message): Promise<any> {
-    try {
-        console.log("Message: \n" + JSON.stringify(message.body, null, 2));
-        // TODO: Do interesting work based on the new message
-        await Promise.resolve(1); //Placeholder for actual async work
-    } catch (err) {
-        console.error("An error occurred");
-        throw err;
-    }
+    console.log("Message: \n" + JSON.stringify(message.body, null, 2));
 }
 
 
