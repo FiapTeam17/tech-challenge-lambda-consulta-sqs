311ecc5320c6133b8293fc53518d19476ceae4cb
Jefferson Nunes <jeffersonvnunes@gmail.com>
2024-03-14 23:22:11 -0300

    fix: url pedido

--- srv/statusPagamento/index.mjs
+++ srv/statusPagamento/index.mjs
@@ -5,7 +5,8 @@ export const handler = async (event, context) => {
     for (const message of event.Records) {
         try {
             console.log("Body: ", message.body);
-            const response = await axios.patch(`${process.env.PEDIDO_API}/pedidos/${message.body.identificador}/${message.body.status}`,
+            const body = JSON.parse(message.body);
+            const response = await axios.patch(`${process.env.PEDIDO_API}/pedidos/${body.identificador}/${body.status}`,
                 message.body,
                 {
                     headers: {
--- srv/statusProducao/index.mjs
+++ srv/statusProducao/index.mjs
@@ -5,7 +5,8 @@ export const handler = async (event, context) => {
     for (const message of event.Records) {
         try {
             console.log("Body: ", message.body);
-            const response = await axios.patch(`${process.env.PEDIDO_API}/pedidos/${message.body.idPedido}/status`,
+            const body = JSON.parse(message.body);
+            const response = await axios.patch(`${process.env.PEDIDO_API}/pedidos/${body.idPedido}/status`,
                 message.body,
                 {
                     headers: {
