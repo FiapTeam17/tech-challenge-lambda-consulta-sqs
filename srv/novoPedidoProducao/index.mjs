6faa5fd85a078c61a48e7ba3e3bab55d8a4ae371
Jefferson Nunes <jeffersonvnunes@gmail.com>
2024-03-14 22:38:31 -0300

    teste

--- srv/novoPedidoProducao/index.mjs
+++ srv/novoPedidoProducao/index.mjs
@@ -4,8 +4,7 @@ export const handler = async (event, context) => {
 
     for (const message of event.Records) {
         try {
-            const data = JSON.stringify(message.body);
-            const response = await axios.post(`${process.env.PRODUCAO_API}/pedidos`, data);
+            const response = await axios.post(`${process.env.PRODUCAO_API}/pedidos`, message.body);
             console.log(response);
         } catch (error) {
             console.error(error);
